494dd19081c2d2c18303168d3f6e30746cdf1476
Priyanshu-Bajpae <143736279+Priyanshu-Bajpae@users.noreply.github.com>
2024-08-30 09:40:05 +0000

    variables and data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+//in javascript semi colon is optional
+const accountId = 1001
+let accountEmail = "hitesh@google.com"
+var accountPassword = "Pb@123"
+accountCity = "Pilani" // not prefered
+let accountState;
+
+// accountId = 1002 // not allowed
+
+console.log(accountId)
+
+accountEmail = "abc@102gmail.com"
+accountCity = "Lucknow"
+accountPassword = "Cb@321"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+/*var vs let on basis of scope
+ forget 'var' and use 'let' and 'const' beacoz of issue in block scope
+
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+// console.log(3+3) console.log("Krsna") // shows error
+// code readibilty should be high
+// documentation for js = https://developer.mozilla.org/en-US/docs/Web/JavaScript
+// ecmaScript for standard 
+
+/* datatypes in js
+
+number
+bigint
+string => "" / ''
+boolean =>true / false
+null=>standalone value
+undefine => let name;
+symbol=> used to identify uniqueness
+
+object
+*/
+console.log(typeof("hare Krsna"))
+console.log(typeof null) //object
+console.log(typeof undefined) // undefined
